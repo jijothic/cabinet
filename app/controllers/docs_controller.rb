4bc1296c464895a8335a7044fdd1da69fe48f847
jijothic <geogtz@yahoo.com>
2016-05-25 18:17:19 +0300

    Doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,39 @@
+# dcoument controller
+class DocsController < ApplicationController
+  def index
+  end
+
+  def show
+    # code
+  end
+
+  def new
+    # code
+  end
+
+  def create
+    # code
+  end
+
+  def edit
+    # code
+  end
+
+  def update
+    # code
+  end
+
+  def destroy
+    # code
+  end
+
+  private
+
+  def find_doc
+    # code
+  end
+
+  def doc_params
+    # code
+  end
+end
